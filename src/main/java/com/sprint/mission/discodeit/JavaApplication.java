b351d3c8e5499f3f5fc4226c0b850922c58a2e77
de31 <sungsam9709@gmail.com>
2025-02-04 09:23:55 +0900

    sprint2

--- src/main/java/com/sprint/mission/discodeit/JavaApplication.java
+++ src/main/java/com/sprint/mission/discodeit/JavaApplication.java
@@ -13,7 +13,7 @@ import java.util.*;
 
 public class JavaApplication {
     public static void main(String[] args) {
-        // Initialize repositories
+
         ChannelRepository jcfChannelRepo = new JCFChannelRepository();
         UserRepository jcfUserRepo = new JCFUserRepository();
         MessageRepository jcfMessageRepo = new JCFMessageRepository();
@@ -22,45 +22,36 @@ public class JavaApplication {
         UserRepository fileUserRepo = new FileUserRepository();
         MessageRepository fileMessageRepo = new FileMessageRepository();
 
-        // Initialize services with JCF repositories
         ChannelService jcfChannelService = new BasicChannelService(jcfChannelRepo);
         UserService jcfUserService = new BasicUserService(jcfUserRepo);
         MessageService jcfMessageService = new BasicMessageService(jcfMessageRepo);
 
-        // Initialize services with File repositories
         ChannelService fileChannelService = new BasicChannelService(fileChannelRepo);
         UserService fileUserService = new BasicUserService(fileUserRepo);
         MessageService fileMessageService = new BasicMessageService(fileMessageRepo);
 
-        // Test JCF services
         System.out.println("Testing JCF Repositories:");
         testServices(jcfChannelService, jcfUserService, jcfMessageService);
 
-        // Test File services
         System.out.println("\nTesting File Repositories:");
         testServices(fileChannelService, fileUserService, fileMessageService);
     }
 
     private static void testServices(ChannelService channelService, UserService userService, MessageService messageService) {
-        // Create user
+
         User user = new User(UUID.randomUUID(), "woody", "woody@codeit.com", "woody1234");
         userService.createUser(user);
 
-        // Create channel
         Channel channel = new Channel(UUID.randomUUID(), ChannelType.PUBLIC, "공지", "공지 채널입니다.");
         channelService.createChannel(channel);
 
-        // Create message
         Message message = new Message(UUID.randomUUID(), "안녕하세요.", channel.getId(), user.getId());
         messageService.createMessage(message);
 
-        // Retrieve all users
         System.out.println("All Users: " + userService.getAllUsers());
 
-        // Retrieve all channels
         System.out.println("All Channels: " + channelService.getAllChannels());
 
-        // Retrieve all messages
         System.out.println("All Messages: " + messageService.getAllMessages());
     }
 }
