6d0da95252175bca9bcbf7ae5ecad5413ca45eb3
de31 <sungsam9709@gmail.com>
2025-06-11 15:31:26 +0900

    feature : 스키마 업데이트

--- src/main/java/com/sprint/mission/discodeit/repository/NotificationRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/NotificationRepository.java
@@ -8,5 +8,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, UUID> {
-  List<Notification> findAllByReceiver_IdOrderByCreatedAtDesc(UUID receiverId);
+  List<Notification> findAllByReceiverIdOrderByCreatedAtDesc(UUID receiverId);
 }
